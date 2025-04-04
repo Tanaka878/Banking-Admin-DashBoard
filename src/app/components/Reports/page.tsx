2cc080d4e271f33b31003628266d24c1703886ff
Tanaka878 <musungaretanaka@gmail.com>
2025-04-04 18:57:38 +0200

    commit

---
+++ src/app/components/Reports/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+
+        Reports Coming soon
+      
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/components/Settings/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+
+        Settings Page coming soon
+      
+    </div>
+  )
+}
+
+export default page
